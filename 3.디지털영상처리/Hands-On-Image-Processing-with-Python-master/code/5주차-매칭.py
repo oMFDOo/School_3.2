c1a3623d818e05417d480f5e3b829c8429e6b9b5
oMFDOo <jinjoo021@naver.com>
2021-10-07 22:15:49 +0900

    영상처리 과제 끝><

---
+++ 3.디지털영상처리/Hands-On-Image-Processing-with-Python-master/code/5주차-매칭.py
@@ -0,0 +1,85 @@
+from matplotlib import pylab as pylab
+
+from skimage.io import imread
+
+from skimage.color import rgb2gray
+
+from skimage.feature import corner_harris, corner_subpix, corner_peaks, match_descriptors, plot_matches
+
+from skimage.transform import warp, SimilarityTransform, AffineTransform, resize
+
+import cv2
+
+import numpy as np
+
+from skimage import data
+
+from skimage.util import img_as_float
+
+from skimage.exposure import rescale_intensity
+
+from skimage.measure import ransac
+
+ 
+
+temple = rgb2gray(img_as_float(imread('../images/temple.jpg')))
+
+image_original = temple.copy()
+
+image_original_gray = rgb2gray(image_original)
+
+affine_trans = AffineTransform(scale=(0.8, 0.9), rotation=0.1, translation=(120, -20))
+
+image_warped = warp(image_original, affine_trans.inverse, output_shape=image_original.shape) # 원래 이미지를 어파인 변환을 통해 결과 얻음
+
+image_warped_gray = rgb2gray(image_warped)
+
+ 
+
+coordinates = corner_harris(image_original_gray)
+
+coordinates[coordinates > 0.01*coordinates.max()] = 1
+
+coordinates_original = corner_peaks(coordinates, threshold_rel=0.0001, min_distance=5)
+
+ 
+
+coordinates = corner_harris(image_warped_gray)
+
+coordinates[coordinates > 0.01*coordinates.max()] = 1
+
+coordinates_warped = corner_peaks(coordinates, threshold_rel=0.0001, min_distance=5)
+
+ 
+
+coordinates_original_subpix = corner_subpix(image_original_gray, coordinates_original, window_size=9)
+
+coordinates_warped_subpix = corner_subpix(image_warped_gray, coordinates_warped, window_size=9)
+
+ 
+
+#코너가 서로 비슷한지 check
+
+matches = match_descriptors(coordinates_original, coordinates_warped, metric="euclidean", cross_check=True) 
+
+#최적의 라인을 찾아감(랜덤이기에 완전 최적은 아님)
+
+#어파인 변화 = model_robust, 대응 관계 점 = inliers에 저장
+
+model_robust, inliers = ransac((coordinates_original[matches[:, 0]], coordinates_warped[matches[:, 1]]), AffineTransform, min_samples=3, residual_threshold=2, max_trials=100)
+
+ 
+
+pylab.figure(figsize=(20,15))
+
+ax = pylab.subplot(111)
+
+plot_matches(ax, image_original_gray, image_warped_gray,
+
+             coordinates_original, coordinates_warped,
+
+             matches[inliers], matches_color ='b')
+
+ax.axis('off')
+
+pylab.show()
---
+++ 3.디지털영상처리/Hands-On-Image-Processing-with-Python-master/code/5주차-해리스.py
@@ -0,0 +1,108 @@
+from matplotlib import pylab as pylab
+from skimage import exposure
+from skimage.io import imread
+from skimage.color import rgb2gray
+import numpy as np
+from scipy import signal
+import cv2
+from skimage.feature import corner_harris, corner_subpix, corner_peaks
+
+#%%
+kernel1d = cv2.getGaussianKernel(3, 1) 
+kernel2d = np.outer(kernel1d, kernel1d.transpose())
+
+def gaussian_kernel(whsize, sigma=1):
+    x, y = np.mgrid[-whsize:whsize+1, -whsize:whsize+1]
+    g_w = np.zeros(x.shape, dtype = np.double)
+    g_w[:] = np.exp(-0.5 * (x**2 / sigma**2 + y**2 / sigma**2))
+    g_w /= 2* np.pi * sigma**2
+    g_w /= np.sum(g_w)    
+    return g_w
+
+im = np.zeros((12,12))
+
+for i in range(3,8):
+     im[i, 3:i+1] = 1
+    
+ker_x = [[-1, 0, 1]]
+ker_y = [[-1], [0], [1]]
+ker_g = gaussian_kernel(1)
+
+im_x = signal.convolve2d(im, ker_x, mode='same')
+im_y = signal.convolve2d(im, ker_y, mode='same')
+im_x2 = im_x ** 2
+im_y2 = im_y ** 2
+im_xy = im_x * im_y
+
+im_gx = signal.convolve2d(im_x2, ker_g, mode='same') 
+im_gy = signal.convolve2d(im_y2, ker_g, mode='same') 
+im_gxy = signal.convolve2d(im_xy, ker_g, mode='same') 
+  
+im_a = ( im_gx * im_gy - im_gxy*im_gxy) - 0.15*( im_gx + im_gy) ** 2
+im_t = np.where(im_a > im_a.max()*0.01, 255, 0)
+
+im_c = np.zeros([12, 12, 3])
+im_o = np.zeros([12, 12, 3])
+im_o[:,:,0] = im_o[:,:,1] = im_o[:,:,2] = np.clip(im, 0, 255)
+coordinates = corner_harris(im, k =0.15)
+im_c[coordinates>0.0001*coordinates.max()]=[255,0,0]
+corner_coordinates = corner_peaks(coordinates)
+coordinates_subpix = corner_subpix(im, corner_coordinates, window_size=13)
+
+pylab.figure(figsize=(30,20))
+pylab.subplot(221), pylab.imshow(im, cmap='gray'), pylab.title('original'), pylab.axis('off')
+pylab.subplot(222), pylab.imshow(im_gx, cmap='gray'), pylab.title('gradient x'), pylab.axis('off')
+pylab.subplot(223), pylab.imshow(im_a, cmap='gray'), pylab.title('gradient y'), pylab.axis('off')
+#pylab.subplot(224), pylab.imshow(im_t, cmap='gray'), pylab.title('result'), pylab.axis('off')
+#pylab.subplot(224), pylab.imshow(im_c), pylab.title('result'), pylab.axis('off')
+    
+pylab.subplot(224), pylab.imshow(im_o, interpolation='nearest')
+pylab.plot(corner_coordinates[:, 1], corner_coordinates[:, 0], 'bo', markersize=5)
+#pylab.plot(coordinates_subpix[:, 1], coordinates_subpix[:, 0], 'r+', markersize=10), pylab.axis('off')
+pylab.tight_layout(), pylab.show()
+
+#%%
+#image = imread('../images/chess_football.png') # RGB image
+
+image = imread('../images/4.jpg') # RGB image
+
+
+image_harris = image.copy()
+image_gray = rgb2gray(image)
+
+coordinates = corner_harris(image_gray, k =0.01)
+#image_harris[coordinates>0.01*coordinates.max()]=[255,0,0,255]
+image_harris[coordinates>0.01*coordinates.max()]=[255,0,0]
+
+corner_coordinates = corner_peaks(coordinates, min_distance=5, threshold_rel=0.01)
+coordinates_subpix = corner_subpix(image_gray, corner_coordinates, window_size=10)
+
+pylab.figure(figsize=(40,20))
+pylab.subplot(211), pylab.imshow(image_harris), pylab.axis('off')
+pylab.subplot(212), pylab.imshow(image, interpolation='nearest')
+pylab.plot(corner_coordinates[:, 1], corner_coordinates[:, 0], 'bo', markersize=5)
+pylab.plot(coordinates_subpix[:, 1], coordinates_subpix[:, 0], 'r+', markersize=10), pylab.axis('off')
+pylab.tight_layout(), pylab.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
