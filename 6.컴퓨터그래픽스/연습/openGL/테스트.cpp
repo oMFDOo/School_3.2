78f1d0c103a18338a9cee79f43435a2b7b47d1a8
oMFDOo <jinjoo021@naver.com>
2021-10-13 01:12:17 +0900

    강의 다들었당~!

--- 6.컴퓨터그래픽스/연습/openGL/테스트.cpp
+++ 6.컴퓨터그래픽스/연습/openGL/테스트.cpp
@@ -1,73 +1,68 @@
 //#include <iostream>
 //using namespace std;
 //
-//int makeOne(int num) {
-//	int turn = 0;
-//	if (num == 1) {
-//		cout << "-------------------\n";
-//		cout << "동작 : 종료";
-//		cout << "num 값 : 1 \n";
-//		cout << "turn 값 : " << turn << endl;
-//		cout << "-------------------\n\n";
-//		return turn + 1;
-//	}
-//	else if ((num - 1) % 3 == 0) {
-//		cout << "-------------------\n";
-//		cout << "동작 : (num - 1) ÷ 3";
-//		cout << "num 값 : 1 \n";
-//		cout << "turn 값 : " << turn << endl;
-//		cout << "-------------------\n\n";
-//
-//		if ((num - 1) / 3 != 1) { turn += 2; }
-//		turn += makeOne((num-1) / 3);
-//	}
-//	else if (num % 3 == 0) {
-//		cout << "-------------------\n";
-//		cout << "동작 : num ÷ 3";
-//		cout << "num 값 : 1 \n";
-//		cout << "turn 값 : " << turn << endl;
-//		cout << "-------------------\n\n";
-//		
-//		if (num/3 != 1) { turn++; }
-//		turn += makeOne(num / 3);
-//	}
-//	else if ((num - 1) % 3 == 0) {
-//		cout << "-------------------\n";
-//		cout << "동작 : num ÷ 3";
-//		cout << "num 값 : 1 \n";
-//		cout << "turn 값 : " << turn << endl;
-//		cout << "-------------------\n\n";
-//
-//		if ((num - 1) / 3 != 1) { turn += 2; }
-//		turn += makeOne((num - 1) / 3);
-//	}
-//	else if (num % 2 == 0) {
-//		cout << "-------------------\n";
-//		cout << "동작 : num ÷ 2";
-//		cout << "num 값 : 1 \n";
-//		cout << "turn 값 : " << turn << endl;
-//		cout << "-------------------\n\n";
-//
-//		if (num/2 != 1) { turn++; }
-//		turn += makeOne(num / 2);
-//	}
-//	else {
-//		cout << "-------------------\n";
-//		cout << "동작 : num - 1";
-//		cout << "num 값 : 1 \n";
-//		cout << "turn 값 : " << turn << endl;
-//		cout << "-------------------\n\n";
-//
-//		if (num-1 != 1) { turn++; }
-//		turn += makeOne(num - 1);
-//	}
+//class FruitSeller {
+//private:
+//    int price;
+//    int amount;
+//    int bank;
+//public:
+//    FruitSeller(int p, int a, int b);
+//    void SalesApples(int cnt);
+//    int HowMuchIsIt();
+//    void ShowSalesResult();
+//};
+//FruitSeller::FruitSeller(int p, int a, int b) : price(p), amount(a), bank(b) {
 //}
+//void FruitSeller::SalesApples(int cnt) {
+//    amount = amount - cnt;
+//    bank = bank + (price * cnt);
+//}
+//int FruitSeller::HowMuchIsIt() {
+//    return price;
+//}
+//void FruitSeller::ShowSalesResult() {
+//    cout << "남은 사과: " << amount << "개" << endl;
+//    cout << "판매 수익: " << bank << "원" << endl;
+//}
+//class FruitBuyer {
+//private:
+//    int money;
+//    int apple;
+//public:
+//    FruitBuyer(int money, int apple);
+//    void BuyApples(FruitSeller& anotherSeller, int money);
+//    void ShowBuyResult();
+//};
+//FruitBuyer::FruitBuyer(int money, int apple) : money(money), apple(apple) {
 //
+//}
+//void FruitBuyer::BuyApples(FruitSeller& anotherSeller, int cnt) {
+//    int price = anotherSeller.HowMuchIsIt();
+//    if (price * cnt > money) {
+//        cout << "잔액 부족" << endl;
+//        return;
+//    }
+//    anotherSeller.SalesApples(cnt);
+//    money -= price * cnt;
+//    apple += cnt;
+//}
+//void FruitBuyer::ShowBuyResult() {
+//    cout << "사과 개수: " << apple << "개" << endl;
+//    cout << "잔액: " << money << "원" << endl;
+//}
+//void P3(void) {
+//    FruitSeller seller(1000, 20, 0);
+//    FruitBuyer buyer(5000, 0);
+//    buyer.BuyApples(seller, 7);
+//
+//    cout << "과일 판매자의 현황" << endl;
+//    seller.ShowSalesResult();
+//
+//    cout << "과일 구매자의 현황" << endl;
+//    buyer.ShowBuyResult();
+//}
 //int main() {
-//	while (777) {
-//		int num, turn; 
-//		cin >> num;
-//		turn = makeOne(num);
-//		cout << "함수 동작 끝. turn 값 : " << turn << endl << endl;
-//	}
+//    P3();
+//    return 0;
 //}
