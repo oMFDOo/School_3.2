26a579d11bf820f91d366f7edccbfbcfc445fc4d
oMFDOo <jinjoo021@naver.com>
2021-11-23 05:07:00 +0900

    Update 카드셔플과제.cpp

--- 7.컴퓨터알고리즘/과제/카드셔플과제.cpp
+++ 7.컴퓨터알고리즘/과제/카드셔플과제.cpp
@@ -53,7 +53,7 @@ void init(int n, int m) {
     sizeM = m;
 }
 
-// 분할정복 : 재귀
+// 분할정복
 bool shuffle_DC(int A[], int B[], int C[], int n, int m) {
     //cout << "n : " << n << "\t m : " << m << endl;
     //cout << "A[n] : " << A[n] << "\t B[m] : " << B[m] << "\t C[n+m] : " << C[n + m] << endl << endl;
@@ -69,40 +69,41 @@ bool shuffle_DC(int A[], int B[], int C[], int n, int m) {
     return result;
 }
 
-// 동적프로그래밍 : 반복문
+// 동적계획법
 bool shuffle_DP(int A[], int B[], int C[], int n, int m) {
     //cout << "n : " << n << "\t m : " << m << endl;
     //cout << "A[n] : " << A[n] << "\t B[m] : " << B[m] << "\t C[n+m] : " << C[n + m] << endl << endl;
     bool result = true;
+
     if (n == 0 && m == 0) { return true; }
+    else if (SS[n][m] == 1) return SS[n][m];
+    else if (n > 0 && shuffle_DP(A, B, C, n - 1, m) && C[n + m] == A[n]) {
+        SS[n][m] = 1;
+        SS[n - 1][m] = 1;
+    }
+    else if (m > 0 && shuffle_DP(A, B, C, n, m - 1) && C[n + m] == B[m]) {
+        SS[n][m] = 1;
+        SS[n][m - 1] = 1;
+    }
+    return SS[1][1];
+    /*if (n == 0 && m == 0) { return true; }
     else if (S[n][m]) return true;
     else if (A[n] == C[n + m] && 0 < n) {
         S[n][m] = true;
-        result = shuffle_DC(A, B, C, n - 1, m);
+        result = shuffle_DP(A, B, C, n - 1, m);
     }
     else if (B[m] == C[n + m] && 0 < m) {
         S[n][m] = true;
-        result = shuffle_DC(A, B, C, n, m - 1);
+        result = shuffle_DP(A, B, C, n, m - 1);
     }
     else return  false;
-    return result;
+    return result;*/
 }
 
-// 메모리제이션 : 재귀
+// 메모리제이션
 bool shuffle_MM(int A[], int B[], int C[], int n, int m) {
     //cout << "n : " << n << "\t m : " << m << endl;
     //cout << "A[n] : " << A[n] << "\t B[m] : " << B[m] << "\t C[n+m] : " << C[n+m] << endl << endl;
-    /*if (n == 0 && m == 0) { return true; }
-    else if (SS[n][m] == 1) return SS[n][m];
-    else if (n > 0 && shuffle_MM(A, B, C, n - 1, m) && C[n + m] == A[n]) {
-        SS[n][m] = 1;
-        SS[n-1][m] = 1;
-    }
-    else if (m > 0 && shuffle_MM(A, B, C, n, m - 1) && C[n + m] == B[m]) {
-        SS[n][m] = 1;
-        SS[n][m-1] = 1;
-    }
-    return SS[1][1];*/
     if (n == 0 && m == 0)  { return true; }
     else if (SS[n][m] != 0) return SS[n][m];
     else if (n > 0 && C[n + m] == A[n]) {
